51df7f9093311ae6f1fd0beba113eff444847a62
marina saraiva <marinasaraiva@hotmail.fr>
2025-04-01 16:17:43 +0200

    Props

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,16 +1,5 @@
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
+function PokemonCard({ pokemon }) {
 
-function PokemonCard() {
-    const pokemon = pokemonList[1];
     return (
         <div>
             <h2>{pokemon.name}</h2>
