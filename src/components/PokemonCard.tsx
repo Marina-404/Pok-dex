b9abda502d1a5f4a67fc6cfe569ea154ab663018
marina saraiva <marinasaraiva@hotmail.fr>
2025-04-01 15:17:04 +0200

    New pokemon Mew whitout image

---
+++ src/components/PokemonCard.tsx
@@ -0,0 +1,27 @@
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
+function PokemonCard() {
+    const pokemon = pokemonList[1];
+    return (
+        <div>
+            <h2>{pokemon.name}</h2>
+            {pokemon.imgSrc ? (
+            <img src= {pokemon.imgSrc} alt={'image de ${pokemon.name}'} 
+            /> 
+            ) : (
+                <p>???</p>
+            )} 
+        </div>
+    );
+  }
+  
+  export default PokemonCard;
